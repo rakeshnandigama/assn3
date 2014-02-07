87e8237e456c83594dbc49a91302e264990305f9
rakesh nandigama <rakest.nandigama1@marist.edu>
2014-02-07 13:38:12 -0500

    lab-3
    Rakesh Nandigama
    CWID: 200 54 070

---
+++ Assign-3.sql
@@ -0,0 +1,61 @@
+﻿-- Rakesh Nandigama  CWID-200-54-070
+-- Lab 3
+-- 2/7/2014
+
+-------------------------------------------------------------------------------
+
+-- 1. List the name and city of agents named Smith.
+
+select name,city
+from agents
+where name='Smith'
+
+--2. List pid, name, and quantity of products costing more than US$1.25.
+
+select pid,name,quantity 
+from products 
+where priceusd>1.25
+
+--3. List the ordno and aid of all orders.
+
+select ordno, aid
+from orders
+
+--4. List the names and cities of customers in Dallas.
+
+select name, city
+from customers
+where city='Dallas'
+
+--5. List the names of agents not in New York and not in Newark.
+
+select name
+from agents
+where city!='New York'
+  and city!='Newark'
+
+--6. List all data for products not in New York or Newark that cost US$1 or more.
+
+select * 
+from products 
+where city not in('New York','Newark') and priceusd >= 1.00
+
+--7. List all data for orders in January or March.
+
+select * 
+from orders
+where mon='jan'
+   or mon='mar'
+
+ --8. List all data for orders in February less than US$100.
+
+ select * 
+ from orders 
+ where mon='feb'
+   and dollars <= 100
+
+ --9. List all orders from the customer whose cid is C001.
+
+ select *
+ from orders 
+ where cid='c001'
